75391efd71edfbb2f1776a0430bf0d87cd2aca6e
tharunika-19 <tharunikabodasu93.6@gmail.com>
2026-04-23 23:32:02 +0530

    calculator program (tara's)

---
+++ calculator program/calc.js
@@ -0,0 +1,25 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+    try{
+        // ❌ eval works but unsafe (still okay for small projects)
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
